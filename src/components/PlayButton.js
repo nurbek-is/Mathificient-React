ada3253222e00ffe103f7ad0e3fd788fb0118dea
nurbek82 <nurbekismailov@Nurbeks-MacBook-Air.local>
2020-08-11 21:20:25 -0700

    added folders for each components

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function PlayButton() {
     return (
-        <button>Play </button>
+        <button>Play</button>
     )
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
-import Main from './Main';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Main from '../components/Main';
 
 function App() {
   return (
