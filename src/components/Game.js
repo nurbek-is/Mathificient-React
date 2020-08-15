5bdfca97ef7672e94217e00d3f31e498732c1333
nurbek82 <nurbekismailov@Nurbeks-MacBook-Air.local>
2020-08-14 19:14:57 -0700

    added game ui features

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import Score from './Score';	
+import Timer from './Timer';	
+import Equation from './Equation';	
+import NumberButton from './NumberButton';	
+import ClearButton from './ClearButton';
+import './Game.css';	
 
 function Game(props) {
 
@@ -6,6 +12,34 @@ return (
     <div>
     Operation is <strong>{props.operation}</strong><br />
     Maximum number is <strong>{props.maxNumber}</strong>.
+    <main className="text-center" id="game-container">
+  <div className="row border-bottom" style={{fontSize:"1.5em"}}>
+    <div className="col px-3 text-left">
+      <Score score="0" />
+    </div>
+    <div  className="col px-3 text-right">
+      <Timer timeLeft="60" />
+    </div>
+  </div>
+  <div className="row text-secondary my-2" id="equation">
+    <Equation question="1 + 1" answer="2" />
+  </div>
+  <div className="row" id="buttons">
+    <div className="col">
+      <NumberButton value="1" />
+      <NumberButton value="2" />
+      <NumberButton value="3" />
+      <NumberButton value="4" />
+      <NumberButton value="5" />
+      <NumberButton value="6" />
+      <NumberButton value="7" />
+      <NumberButton value="8" />
+      <NumberButton value="9" />
+      <NumberButton value="0" />
+      <ClearButton />
+    </div>
+  </div>
+</main>
     </div>
     )
 }
---
+++ src/components/Timer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Timer (props) {
+return (
+<strong>Time: {props.timeLeft}</strong>
+)
+}
+
+export default Timer 
